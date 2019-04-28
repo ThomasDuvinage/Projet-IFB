71c8b3a8b2195787694674a611f0b5257bcbcb39
totordudu <43036801+totordudu@users.noreply.github.com>
2019-04-28 17:19:06 +0200

    Update main.c

--- Read_csv/main.c
+++ Read_csv/main.c
@@ -4,21 +4,39 @@
 
 #define BSIZE 80
 
+int recherche_salle();
+
 int main()
 {
-	char filename[] = "main.csv";
+
+    for(int i = 219; i<221;i++){
+        char nom_salle[10] = "p"; //on definit le nom de base soit p
+        char nb_salle[12]; // on creer une chaine de caracteres qui va permettre de recevoir le numero de la salle en caracteres
+    
+        int numero_salle = i; // numero de la salle que nous recherchons 
+        sprintf(nb_salle, "%d", numero_salle);//on convertit l'entier numero salle en char dans la chaine de caracteres nb_salle
+        strcat(nom_salle,nb_salle); //on concatene les deux chaines de caracteres
+        strcat(nom_salle,".csv"); //on ajoute la description du fichier 
+        printf("%s\n",nom_salle);
+        recherche_salle(nom_salle);// puis on utilise le nom de la salle dans la fonction
+    }
+
+}
+
+
+int recherche_salle(char nom_salle[10]){
 	char buffer[BSIZE];
 	FILE *f;
 	char *field;
 	int DISPO0,DISPO15,DISPO30,DISPO45;
 
-    int n =8;
+    int n = 8;
 
 	/* open the CSV file */
-	f = fopen(filename,"r");
+	f = fopen(nom_salle,"r");
 	if( f == NULL)
 	{
-		printf("Unable to open file '%s'\n",filename);
+		printf("Unable to open file '%s'\n",nom_salle);
 		exit(1);
 	}
 
@@ -32,10 +50,10 @@ int main()
 		DISPO15=atoi(field);
 
 		field=strtok(NULL,",");
-		DISPO30=atof(field);
+		DISPO30=atoi(field);
 
         field=strtok(NULL,",");
-		DISPO45=atof(field);
+		DISPO45=atoi(field);
 
 
 		/* display the result in the proper format */
