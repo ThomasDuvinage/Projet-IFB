747a1da5ac67beee8e7453cd912c547ba51b226d
totordudu <43036801+totordudu@users.noreply.github.com>
2019-04-29 14:02:53 +0200

    print day

--- time_example/example.c
+++ time_example/example.c
@@ -10,6 +10,18 @@ int main(void)
     instant=*localtime(&secondes);
 
     printf("%d/%d ; %d:%d:%d\n", instant.tm_mday+1, instant.tm_mon+1, instant.tm_hour, instant.tm_min, instant.tm_sec);
+
+    char format[128];
+    time_t temps;
+    struct tm date;
+
+    // On récupère la date et l'heure actuelles.
+    time(&temps);
+    date=*localtime(&temps);
+
+    // On remplit la chaîne avec le format choisi, puis on l'affiche.
+    strftime(format, 128, "%A \n", &date);
+    puts(format);
     return 0;
 }
 
