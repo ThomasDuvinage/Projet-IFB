ba31586c3caf85c7a2a5295f62fed3fddd42c9a5
totordudu <43036801+totordudu@users.noreply.github.com>
2019-04-29 15:01:04 +0200

    generate_timetable

--- generate_timetable/generate_timetable.c
+++ generate_timetable/generate_timetable.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #define BSIZE 80
+
+int recherche_salle();
 
 void generate();
 
@@ -18,17 +21,43 @@ int main()
         strcat(nom_salle,nb_salle); //on concatene les deux chaines de caracteres
         strcat(nom_salle,".csv"); //on ajoute la description du fichier 
         printf("%s\n",nom_salle);
+        printf("%d \n",recherche_salle(nom_salle,9,0));
 
-        generate(nom_salle);// puis on utilise le nom de la salle dans la fonction
     }
 }
 
 
-void generate(char nom_salle[10]){
+void generate(){
 	char buffer[BSIZE];
 	FILE *f;
 	char *field;
-	int DISPO0,DISPO15,DISPO30,DISPO45;//variables correspondant a la dispo pour chaque 1/4 H
+	int DISPO_E1[20]={0},DISPO_E2[20]={0},DISPO_E3[20]={0};//variables correspondant a la dispo pour chaque 1/4 H
+
+    char day[128];
+    time_t temps;
+    struct tm date;
+
+    // On récupère la date et l'heure actuelles.
+    time(&temps);
+    date=*localtime(&temps);
+
+    // On remplit la chaîne avec le format choisi, puis on l'affiche.
+    strftime(day, 128, "%A", &date);//%A permet de renvoyer exclusivement le jour (Monday,..)
+    //puts(format);
+    
+    
+	
+	
+
+}
+
+
+
+int recherche_salle(char nom_salle[10],int heure,int minute){//les minutes --> 0, 15, 30, 45
+	char buffer[BSIZE];
+	FILE *f;
+	char *field;
+	int DISPO0,DISPO15,DISPO30,DISPO45;
 
 	int DISPO_J[40]={0};//tableau qui permet d'acceuillir toutes les valeurs que nous lisons du fichier 
 	//cela permet de pouvoir modifier le tableau puis de le l'ecrire par la suite dans le fichier
@@ -74,22 +103,12 @@ void generate(char nom_salle[10]){
         //n++; //utile pour l'affichage de l'heure 
 	}
 	/* close file */
-	fclose(f);//on ferme la boucle de lecture de fichier 
+	fclose(f);//femeture de la lecture du fichier
 
-	//il faut remplacer à cette endroit les valeurs du tableau afin de les remettre dans le fichier 
+	int index = ((heure-8)*4)+(minute/15);
 
-	
+	//printf("%d \n",index);
+	//printf("%d \n",DISPO_J[index]);
 
-	//on passe en mode ecriture de fichier, cela permet de remplacer les valeurs dans le tableau et apres de les inseres dans le fichier
-	f = fopen(nom_salle,"w");
-
-	//on fait une boucle pour remplir le fichier 
-	for(int i = 0; i < 40; i=i+4)//40 correspond au nombre de valeur car on fait de 8h a 18h donc 4*10 valeurs 
-	{
-		fprintf(f, "%d,%d,%d,%d\n",DISPO_J[i],DISPO_J[i+1],DISPO_J[i+2],DISPO_J[i+3]);//on ecrit dans le fichier les valeurs du tableau que nous avons modofié ou non 
-	}
-	
-	fclose(f);
-
-	return(0);
+	return(DISPO_J[index]);
 }
