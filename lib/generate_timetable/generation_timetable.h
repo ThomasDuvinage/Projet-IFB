052b2e0fffa0359e4b4696ead6cc4f9f9d00f305
totordudu <43036801+totordudu@users.noreply.github.com>
2019-05-06 11:35:02 +0200

    update

--- lib/generate_timetable/generation_timetable.h
+++ lib/generate_timetable/generation_timetable.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef GENERATION_TIMETABLE_H_INCLUDE
+#define GENERATION_TIMETABLE_H_INCLUDE
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,16 +8,42 @@
 #include <math.h>
 
 
-#define BSIZE 80
 #define NB_SIZE 100
 
 int DISPO_E1[NB_SIZE]={0},DISPO_E2[NB_SIZE]={0},DISPO_E3[NB_SIZE]={0};//variables correspondant a la dispo pour chaque 1/4 H
 
 //nous definisons les fonctions
-int recherche_salle(int etage,int numero_salle,char jour[],int heure,int minute);
-void generate(int nb_employe,int DISPO_E1[20],int DISPO_E2[20],int DISPO_E3[20]);
+int 
+recherche_salle(int etage,int numero_salle,char jour[],int heure,int minute);
 
-//declaration de la fonction pour le calcul de l'ecart type 
-float ecart_type(int DIPO_ETAGE[]);
-int long_DISPO_ETAGE(int chaine[]);
+/**
+ * @brief Cette fonction permet de génerer l'emploi du temps de chaque personne 
+ * 
+ * @param nb_employe 
+ * @param DISPO_E1 
+ * @param DISPO_E2 
+ * @param DISPO_E3 
+ */
+void 
+generate(int nb_employe,int DISPO_E1[20],int DISPO_E2[20],int DISPO_E3[20]);
 
+/**
+ * @brief Cette fonction permet de savoir a quelle heure les salles sont le plus libre en fonction de l'heure cela permet de savoir le nombre moyens que nous devons mettre a chaque etage afin de mieux manager le tout 
+ * 
+ * @param DIPO_ETAGE 
+ * @return float 
+ */
+float 
+ecart_type(int DIPO_ETAGE[]);
+
+/**
+ * @brief permet d'avoir la longueur de la chaine 
+ * 
+ * @param chaine 
+ * @return int 
+ */
+int 
+long_DISPO_ETAGE(int chaine[]);
+
+
+#endif
